9caa6e401e393267f0d7539eaf53c27e03d3aa88
recatek <recatek@hotmail.com>
2022-11-19 00:11:00 -0500

    Reverting to original simple_insert for naive

--- src/naive/simple_insert.rs
+++ src/naive/simple_insert.rs
@@ -19,24 +19,25 @@ struct World {
     velocities: Vec<Velocity>,
 }
 
-pub struct Benchmark(World);
+pub struct Benchmark;
 
 impl Benchmark {
     pub fn new() -> Self {
-        Self(World {
+        Self
+    }
+
+    pub fn run(&mut self) {
+        let mut world = World { 
             transforms: Vec::with_capacity(10000),
             positions: Vec::with_capacity(10000),
             rotations: Vec::with_capacity(10000),
             velocities: Vec::with_capacity(10000),
-        })
-    }
+        };
 
-    pub fn run(&mut self) {
-        for _ in 0..10000 {
-            self.0.transforms.push(Transform(Matrix4::from_scale(1.0)));
-            self.0.positions.push(Position(Vector3::unit_x()));
-            self.0.rotations.push(Rotation(Vector3::unit_x()));
-            self.0.velocities.push(Velocity(Vector3::unit_x()));
-        }
+        // Capture the actual insertion process, rather than initializing as-is
+        world.transforms.extend(vec![Transform(Matrix4::from_scale(1.0)); 10000]);
+        world.positions.extend(vec![Position(Vector3::unit_x()); 10000]);
+        world.rotations.extend(vec![Rotation(Vector3::unit_x()); 10000]);
+        world.velocities.extend(vec![Velocity(Vector3::unit_x()); 10000]);
     }
 }
